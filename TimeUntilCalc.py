bcf5596721fd9d60dcd5f819f8de0ebf7531702e
TaeUpNext <armontaeh17@gmail.com>
2020-12-29 15:59:45 -0500

    finished time until 2 dates

--- TimeUntilCalc.py
+++ TimeUntilCalc.py
@@ -21,6 +21,9 @@ while starting == True:
   total_minutes = total_seconds / 60 #*Converts the seconds into minutes
   total_hours = total_minutes / 60#*Convers the minutes into hours
   total_days = total_hours / 24 #*converts the hours into days
+  total_weeks = total_days / 7 #*Converts the days into weeks 
+  total_months = total_weeks / 4.34 #*Converts the week into months
+  total_years = total_months / 12 #*Converts the months into years
 
   
   print(formattedStartDate) #*displays the inputted start date in Month/dd/yyyy format
@@ -39,7 +42,7 @@ while starting == True:
         determineConversion = eval(input("\nHow would you like to convert the time? Enter a 1 for seconds, a 2 for minutes, a 3 for hours, a 4 for days, a 5 for weeks, a 6 for months or a 7 for years: ")) #*Determines how to convert the time between the 2 dates
         #!Time until formula for seconds
         if determineConversion == 1: 
-          print(f"\nThere is {total_seconds} seconds until {fEndDate}") #*Prints the time in seconds until the end date
+          print(f"\nThere is {total_seconds:,.2f} seconds until {fEndDate}") #*Prints the time in seconds until the end date
           rConversionExcpetion = False #*Exception handling for the redoing 
           
           while rConversionExcpetion == False: #*Exception handling to determine if you would like to redo the conversion
@@ -71,7 +74,7 @@ while starting == True:
           
         #!Time Until Minutes
         if determineConversion == 2: 
-          print(f"\nThere is {total_minutes} minutes until {fEndDate}") #*Prints the time in seconds until the end date
+          print(f"\nThere is {total_minutes:,.2f} minutes until {fEndDate}") #*Prints the time in seconds until the end date
           rConversionExcpetion = False #*Exception handling for the redoing 
           while rConversionExcpetion == False: #*Exception handling to determine if you would like to redo the conversion
             redoConversion = eval(input("\nwould you like to convert the time into another value? Enter a 1 for yes or a 2 for no: ")) 
@@ -102,7 +105,7 @@ while starting == True:
         
         #!Time Until Hours
         if determineConversion == 3: 
-          print(f"\nThere is {total_hours} hours until {fEndDate}") #*Prints the time in seconds until the end date
+          print(f"\nThere is {total_hours:,.2f} hours until {fEndDate}") #*Prints the time in seconds until the end date
           rConversionExcpetion = False #*Exception handling for the redoing 
           while rConversionExcpetion == False: #*Exception handling to determine if you would like to redo the conversion
             redoConversion = eval(input("\nwould you like to convert the time into another value? Enter a 1 for yes or a 2 for no: ")) 
@@ -133,7 +136,7 @@ while starting == True:
         
         #!Time Until Days
         if determineConversion == 4: 
-          print(f"\nThere is {total_days} hours until {fEndDate}") #*Prints the time in seconds until the end date
+          print(f"\nThere is {total_days:,.2f} day/days until {fEndDate}") #*Prints the time in seconds until the end date
           rConversionExcpetion = False #*Exception handling for the redoing 
           while rConversionExcpetion == False: #*Exception handling to determine if you would like to redo the conversion
             redoConversion = eval(input("\nwould you like to convert the time into another value? Enter a 1 for yes or a 2 for no: ")) 
@@ -161,3 +164,98 @@ while starting == True:
                 else:
                   print("\nYou entered an invalid input, please try again: ")
                   newDateConversionException = False
+                  
+        #!Time Until Weeks
+        if determineConversion == 5: 
+          print(f"\nThere is {total_weeks:,.2f} week/weeks until {fEndDate}") #*Prints the time in seconds until the end date
+          rConversionExcpetion = False #*Exception handling for the redoing 
+          while rConversionExcpetion == False: #*Exception handling to determine if you would like to redo the conversion
+            redoConversion = eval(input("\nwould you like to convert the time into another value? Enter a 1 for yes or a 2 for no: ")) 
+            if redoConversion == 1:#*user wants to convert into another time 
+              dConversionException = False #*Sets the determine conversion loop to False to reneter the loop
+              rConversionExcpetion = True #*Approves the redo conversion exception
+            elif redoConversion == 2: #*user does not want to redo  a conversion conversion
+              rConversionExcpetion = True #*approves the redo conversion excpetion
+              newDateConversionException = False #
+              
+              while newDateConversionException == False: #*Exception handling to determine if you would like to enter a new date
+                redoDate= eval(input("\nwould you like to enter a new date? Enter a 1 for yes or a 2 for no: "))
+                if redoDate == 1: #*User wants to enter a new date
+                  starting = True #*sets the date loop to true so user can enter a new one
+                  rConversionExcpetion = True
+                  dConversionException = True
+                  dTimeException = True
+                  newDateConversionException = True
+                elif redoConversion == 2: #*user would not like to enter a new date
+                  starting = False
+                  rConversionExcpetion = True
+                  dConversionException = True #*exits the determine conversion loop
+                  dTimeException = True
+                  newDateConversionException = True
+                else:
+                  print("\nYou entered an invalid input, please try again: ")
+                  newDateConversionException = False
+        
+        #!Time until months
+        if determineConversion == 6: 
+          print(f"\nThere is {total_months:,.2f} month/months until {fEndDate}") #*Prints the time in seconds until the end date
+          rConversionExcpetion = False #*Exception handling for the redoing 
+          while rConversionExcpetion == False: #*Exception handling to determine if you would like to redo the conversion
+            redoConversion = eval(input("\nwould you like to convert the time into another value? Enter a 1 for yes or a 2 for no: ")) 
+            if redoConversion == 1:#*user wants to convert into another time 
+              dConversionException = False #*Sets the determine conversion loop to False to reneter the loop
+              rConversionExcpetion = True #*Approves the redo conversion exception
+            elif redoConversion == 2: #*user does not want to redo  a conversion conversion
+              rConversionExcpetion = True #*approves the redo conversion excpetion
+              newDateConversionException = False #
+              
+              while newDateConversionException == False: #*Exception handling to determine if you would like to enter a new date
+                redoDate= eval(input("\nwould you like to enter a new date? Enter a 1 for yes or a 2 for no: "))
+                if redoDate == 1: #*User wants to enter a new date
+                  starting = True #*sets the date loop to true so user can enter a new one
+                  rConversionExcpetion = True
+                  dConversionException = True
+                  dTimeException = True
+                  newDateConversionException = True
+                elif redoConversion == 2: #*user would not like to enter a new date
+                  starting = False
+                  rConversionExcpetion = True
+                  dConversionException = True #*exits the determine conversion loop
+                  dTimeException = True
+                  newDateConversionException = True
+                else:
+                  print("\nYou entered an invalid input, please try again: ")
+                  newDateConversionException = False
+        
+        #!Time Until years
+        if determineConversion == 7: 
+          print(f"\nThere is {total_years:,.2f} year/years until {fEndDate}") #*Prints the time in seconds until the end date
+          rConversionExcpetion = False #*Exception handling for the redoing 
+          while rConversionExcpetion == False: #*Exception handling to determine if you would like to redo the conversion
+            redoConversion = eval(input("\nwould you like to convert the time into another value? Enter a 1 for yes or a 2 for no: ")) 
+            if redoConversion == 1:#*user wants to convert into another time 
+              dConversionException = False #*Sets the determine conversion loop to False to reneter the loop
+              rConversionExcpetion = True #*Approves the redo conversion exception
+            elif redoConversion == 2: #*user does not want to redo  a conversion conversion
+              rConversionExcpetion = True #*approves the redo conversion excpetion
+              newDateConversionException = False #
+              
+              while newDateConversionException == False: #*Exception handling to determine if you would like to enter a new date
+                redoDate= eval(input("\nwould you like to enter a new date? Enter a 1 for yes or a 2 for no: "))
+                if redoDate == 1: #*User wants to enter a new date
+                  starting = True #*sets the date loop to true so user can enter a new one
+                  rConversionExcpetion = True
+                  dConversionException = True
+                  dTimeException = True
+                  newDateConversionException = True
+                elif redoConversion == 2: #*user would not like to enter a new date
+                  starting = False
+                  rConversionExcpetion = True
+                  dConversionException = True #*exits the determine conversion loop
+                  dTimeException = True
+                  newDateConversionException = True
+                else:
+                  print("\nYou entered an invalid input, please try again: ")
+                  newDateConversionException = False
+                  
+
